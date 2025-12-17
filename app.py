9b619cc73f47dd302ba766e17ba0b899a087d1ef
bhavya11720-art <bhavya.11720@gmail.com>
2025-12-17 14:39:18 +0530

    Comment out model loading for prediction app
    The model loading line is commented out, which disables model predictions.

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pickle
 import numpy as np
 
 # Load the trained model
-with open('model.pkl', 'rb') as file:
-    model = pickle.load(file)
+#with open('model.pkl', 'rb') as file:
+    #model = pickle.load(file)
 
 # App title
 st.title("🩺 Logistic Regression Diabetes Prediction App")
@@ -35,6 +35,7 @@ if st.button("Predict"):
         st.error("The person is **diabetic**")
     else:
         st.success("The person is **not diabetic**")
+
 
 
 
